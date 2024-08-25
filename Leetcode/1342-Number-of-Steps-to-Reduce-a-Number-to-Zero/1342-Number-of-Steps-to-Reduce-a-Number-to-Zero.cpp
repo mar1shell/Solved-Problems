cd3bfa89c66c625c7a12de700be4364c933ce68e
mar1shell <elhamdaouimar1@gmail.com>
2024-08-25 13:40:25 +0100

    added solution folder for problem 1342 in Leetcode

---
+++ Leetcode/1342-Number-of-Steps-to-Reduce-a-Number-to-Zero/1342-Number-of-Steps-to-Reduce-a-Number-to-Zero.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int count = 0;
+        
+        while (num) {
+            if (num % 2 == 0) {
+                num /= 2;
+                count++;
+            } else {
+                num--;
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
