8542dc5d72d9f5df429f65003449debcdeeaee78
mar1shell <elhamdaouimar1@gmail.com>
2024-09-07 16:02:59 +0100

    added solution folder for problem 709 in Leetcode

---
+++ Leetcode/709-To-Lower-Case/709-To-Lower-Case.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] >= 'A' && s[i] <= 'Z') {
+                s[i] += 'a' - 'A';
+            }
+        }
+
+        return s;
+    }
+};
